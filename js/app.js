2e67edab57c845417008f37f1152cb3f0d18c1d2
tanjid111 <tanjidhossain@gmail.com>
2022-02-28 23:01:42 -0800

    features added

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ const displayPhone = (phones) => {
     }
     else {
         first20Phones.forEach(phone => {
-            console.log(phone);
+            // console.log(phone);
             document.getElementById('error-message2').style.display = 'none';
             const div = document.createElement('div');
             div.classList.add('col');
@@ -37,16 +37,73 @@ const displayPhone = (phones) => {
                 <img src="${phone.image}" class="card-img-top w-75" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${phone.brand}</h5>
-                    <p class="card-text">${phone.phone_name}</p>
-                    <button onclick="phoneDetails('phone.slug')" class="btn btn-primary">See Details</button>
+                    <h5 class="card-text">${phone.phone_name}</h5>
+                    <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">See Details</button>
                 </div>
             </div>
             `;
             searchResult.appendChild(div);
         })
-
     }
+}
+
+phoneDetails = (phoneId) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(phoneData => displayPhoneDetail(phoneData.data))
+}
+
+const displayPhoneDetail = (phoneInfo) => {
+    console.log(phoneInfo);
+    const phoneDetail = document.getElementById('phone-detail');
+    phoneDetail.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    // div.classList.add('text-center');
+
+    div.innerHTML = `
+    <img src="${phoneInfo.image}" class="card-img-top pt-2 w-25 mx-auto" alt="...">
+            <div class="card-body">
+                    <h5 class="card-title">${phoneInfo.brand}</h5>
+                    <h5 class="card-title">${phoneInfo.name}</h5>
+                    <p>Released date: noReleaseDate(${phoneInfo.releaseDate})</p>
+                    <p id="error-message3" class="text-danger">No release date found</p>
+                <div class="row row-cols-md-2">
+                    <div class="col">
+                        <h5 class="card-title">Features:</h5>
+                        <div>
+                            <ul class="list-group">
+                                <li class="list-group-item">${phoneInfo.mainFeatures.chipSet}</li>
+                                <li class="list-group-item">${phoneInfo.mainFeatures.displaySize}</li>
+                                <li class="list-group-item">${phoneInfo.mainFeatures.memory}</li>
+                                <li class="list-group-item">${phoneInfo.mainFeatures.storage}</li>               
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <h5 class="card-title">Other Features:</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item">${phoneInfo.others.Bluetooth}</li>
+                            <li class="list-group-item">${phoneInfo.others.GPS}</li>
+                            <li class="list-group-item">${phoneInfo.others.NFC}</li>
+                            <li class="list-group-item">${phoneInfo.others.Radio}</li>
+                            <li class="list-group-item">${phoneInfo.others.USB}</li>
+                            <li class="list-group-item">${phoneInfo.others.WLAN}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    `
+    phoneDetail.appendChild(div);
+
+    if (phoneInfo.releaseDate === 0) {
+        document.getElementById('error-message3').style.display = 'block';
+    }
+
 }
 //display all phones function is defined
 // const searchPhoneAll = (phones) => {
 // }
+{/* <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a> */}
